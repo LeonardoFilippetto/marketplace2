847137d5a82859373c1dffd641fa427f16d7b255
LeonardoFilippetto <124211664+LeonardoFilippetto@users.noreply.github.com>
2023-08-21 22:07:11 -0300

    criação de DTOs e DAOs

---
+++ model/AnuncioDAO.php
@@ -0,0 +1,110 @@
+<?php
+require_once('..\bd\gerenciador_de_conexoes.php');
+require_once('AnuncioDTO.php');
+
+class AnuncioDAO{
+	private $con;
+
+	function __construct()
+	{
+		$this->con = GerenciadorDeConexoes::obter_conexao();
+	}
+
+	function inserir($anuncio){
+		$result = $this->con->query("INSERT INTO anuncios (id_produto, id_vendedor, titulo_anuncio, categoria_produto, preco, estoque, img_princ, imgs_sec, descricao, informacoes_adicionais, ativo, vendas_registradas) VALUES ('" . $anuncio->get_id_produto() . "', '" . $anuncio->get_id_vendedor() . "', '" . $anuncio->get_titulo_anuncio() . "', '" . $anuncio->get_categoria_produto() . "', '" . $anuncio->get_preco() . "', '" . $anuncio->get_estoque() . "', '" . $anuncio->get_img_princ() . "', '" . $anuncio->get_imgs_sec() . "', '" . $anuncio->get_descricao() . "', '" . $anuncio->get_informacoes_adicionais() . "', '" . $anuncio->get_ativo() . "', '" . $anuncio->get_vendas_registradas() . "')");
+   		
+   		if ($result->rowCount() > 0){
+   			return true;
+   		}
+   		else{
+   			return false;
+   		}
+	}
+
+	function alterar($anuncio){
+		$result = $this->con->query("UPDATE anuncios SET id_produto = '" . $anuncio->get_id_produto() . "', id_vendedor = '" . $anuncio->get_id_vendedor() . "', titulo_anuncio = '" . $anuncio->get_titulo_anuncio() . "', categoria_produto = '" . $anuncio->get_categoria_anuncio() . "', preco = '" . $anuncio->get_preco() . "', estoque = '" . $anuncio->get_estoque() . "', img_princ = '" . $anuncio->get_img_princ() . "', imgs_sec = '" . $anuncio->get_imgs_sec() . "', descricao = '" . $anuncio->get_descicao() . "', informacoes_adicionais = '" . $anuncio->get_informacoes_adicionais() . "', ativo = '" . $anuncio->get_ativo() . "', vendas_registradas = '" . $anuncio->get_vendas_registradas() . "' WHERE (id_anuncio = " . $anuncio->get_id_anuncio(). ")");
+
+		if ($result->rowCount() > 0){
+   			return true;
+   		}
+   		else{
+   			return false;
+   		}
+	}
+
+	function excluir($id){  
+		$result = $this->con->query("DELETE FROM anuncios WHERE (id_anuncio = '" . $id . "')");
+
+		if ($result->rowCount() > 0){
+   			return true;
+   		}
+   		else{
+   			return false;
+   		}
+	}
+
+	function obter($id){
+		$result =$this->con->query("SELECT * FROM anuncios WHERE (id_anuncio = " . $id . ");");
+		$row = $result->fetch(PDO::FETCH_ASSOC);
+
+		$a = new Anuncio();
+		$a ->set_id_anuncio($row['id_anuncio']);
+		$a ->set_id_produto($row['id_produto']);
+		$a ->set_id_vendedor($row['id_vendedor']);
+		$a ->set_titulo_anuncio($row['titulo_anuncio']);
+		$a ->set_categoria_produto($row['categoria_produto']);
+		$a ->set_preco($row['preco']);
+		$a ->set_estoque($row['estoque']);
+		$a ->set_img_princ($row['img_princ']);
+		$a ->set_imgs_sec($row['imgs_sec']);
+		$a ->set_descricao($row['descricao']);
+		$a ->set_informacoes_adicionais($row['informacoes_adicionais']);
+		$a ->set_ativo($row['ativo']);
+		$a ->set_vendas_registradas($row['vendas_registradas']);
+		
+		return $a;
+	}
+
+	function obter_todos(){
+		$lista = [];
+		$result =$this->con->query("SELECT * FROM anuncios;");
+ 
+		while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+			$a = new anuncio();
+		    $a ->set_id_anuncio($row['id_anuncio']);
+			$a ->set_id_produto($row['id_produto']);
+			$a ->set_id_vendedor($row['id_vendedor']);
+			$a ->set_titulo_anuncio($row['titulo_anuncio']);
+			$a ->set_categoria_produto($row['categoria_produto']);
+			$a ->set_preco($row['preco']);
+			$a ->set_estoque($row['estoque']);
+			$a ->set_img_princ($row['img_princ']);
+			$a ->set_imgs_sec($row['imgs_sec']);
+			$a ->set_descricao($row['descricao']);
+			$a ->set_informacoes_adicionais($row['informacoes_adicionais']);
+			$a ->set_ativo($row['ativo']);
+			$a ->set_vendas_registradas($row['vendas_registradas']);
+			array_push($lista, $a);
+		}
+
+		return $lista;
+	}
+
+    /*function obter_por_nome($nome){
+		$lista = [];
+		$result = $this->con->query("SELECT codigo, nome, idade FROM cliente WHERE (nome like '%" . $nome . "%');");
+ 
+		while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+			$c = new Exemplo();
+			$c->set_codigo($row['codigo']);
+			$c->set_nome($row['nome']);
+			$c->set_idade($row['idade']);
+			array_push($lista, $c);
+		}
+
+		return $lista;
+	}*/
+
+}
+
+?>
---
+++ view/cadastro_anuncio_produto.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" type="text/css" href="css/cadastro.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-... (hash)" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <title>Dados do produto</title>
+    <!-- <script defer src="js/cadastro_final.js"></script> -->
+    <style>
+        form{
+            width: 900px;
+        }
+    </style>
+</head>
+<body>
+<header>
+    <a class="btn-voltar" href="meus_anuncios.php"> <h1><i class="fa-solid fa-arrow-left fa-lx" style="color: #ffffff;"></i> Voltar</h1></a>
+</header>
+    <form method="POST" action="cadastro_anuncio_info_adic.php">
+        <div class="cadastre-se">
+                <div class="entrar-items">
+                  <label for="ean">EAN (código de barras):*</label>
+                    <input type="number" onKeyPress="if(this.value.length==13) return false;" placeholder="1234567891234" id="ean" name="ean" required>
+                    <p id="mens_ean" class="mens"></p>
+                </div>
+                <?php echo $form_produto; ?>
+                <p style="font-size:12px; color:#a6a6a6;">(*) - Campos obrigatórios</p><br>
+                <div class="btn-cad justify"><input type="submit" value="Prosseguir"></div>
+            </div>
+        </div>
+    </form>
+    <footer>
+        <strong><p>Copyright © 2023 StockPC Inc. Todos os direitos reservados.</p></strong>
+    </footer>  
+    <script src="https://kit.fontawesome.com/9e879c63ad.js" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
